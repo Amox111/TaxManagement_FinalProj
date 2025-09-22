c9f5202edffece8de7265862f03bf7c1a2d2377e
Amooraaa <63004019+Amox111@users.noreply.github.com>
2025-09-23 00:12:35 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+void loadCSV(){
+    FILE *file = fopen("Information.csv", "r")
+    if (file == NULL) {
+        print("Can not open file CVS\n");
+        return;
+    }
+
+    while 
+
+
+
+int main(){
+    int Choose;
+    loadCVS();
+
+
+
+
+
+    return 0;
+}
+
+ 
+}
