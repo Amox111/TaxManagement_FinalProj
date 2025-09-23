a2f90b4cc3dbe0f73f50f6d8b3c045bc4f6d00fc
Amooraaa <63004019+Amox111@users.noreply.github.com>
2025-09-23 15:05:22 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
 #include<stdio.h>
 #include<string.h>
 
+//Structure - use struct bc contain differnt date type
+struct Payment
+{
+    char paymentID[10];
+    char payerName[30];
+    char taxType[20];
+    int amount;
+    char paymentDate[11];
+};
+
+//Display Menu
+void displayMenu(){
+    printf("====Tax Mangenment====\n");
+    printf("1.Add the information\n");
+    printf("2.Update the information\n");
+    printf("3.Delete \n");
+    printf("4.Search(by paymentID)\n");
+    printf("5.Exit\n");
+    printf("Enter your choice:");
+}
+
 void loadCSV(){
-    FILE *file = fopen("Information.csv", "r")
+    FILE *file = fopen("record.csv", "w")
     if (file == NULL) {
-        print("Can not open file CVS\n");
+        print("Can not open file\n");
         return;
     }
+    fscanf(file,"paymentID,payerName,taxType,amount,paymentData");
+    
 
-    while 
 
 
 
 int main(){
-    int Choose;
-    loadCVS();
+    displayMenu();
 
 
 
@@ -24,4 +45,3 @@ int main(){
 }
 
  
-}
