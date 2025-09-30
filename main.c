a36a00b897335a4b29e276078cf0e0a3f3f72be9
Amooraaa <63004019+Amox111@users.noreply.github.com>
2025-09-30 21:45:02 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
 #include<stdio.h>
 #include<string.h>
 
-//Structure 
+#define MAX_RECORDS 100
+//unit test and end to end test
+
+typedef struct{
     char paymentID[10];
     char payerName[30];
     char taxType[20];
-    int amount;
-    char paymentDate[11];
+    double amount;
+    char paymentDate[11]; //yyy-mm-dd
+ } paymentRecord ;
 
-//Display Menu
-void displayMenu(){
-    printf("====Tax Mangenment====\n");
-    printf("1.Add the information\n");
-    printf("2.Update the information\n");
-    printf("3.Delete \n");
-    printf("4.Search(by paymentID)\n");
-    printf("5.Exit\n");
-    printf("Enter your choice:");
+ paymentRecord records[MAX_RECORDS];
+ int recordCount = 0;
+
+void saveCSV(){
+    FILE *file = fopen("record.csv", "w");
+    if (file == NULL) {
+        printf("Can not open file\n");
+        return;
+    }    
+
+    for (int i=0; i<recordCount; i++){
+        fprintf(file,"%S,%S,%S,%.2f,%s\n", records[i].paymentID, records[i].payerName, records[i].taxType, records[i].amount, records[i].paymentDate);
+    }   
+    fclose(file);
+    printf("Saved successfully");
 }
 
 void readCSV(){
@@ -25,29 +35,20 @@ void readCSV(){
         printf("Can not open file\n");
         return;
     }
-/*  while (fscanf(file,"%S,%s,%s,%d,%d\n",paymentID,payerName,taxType,&amount,paymentData) !== EOF)
     fclose(file);
-    return;
-}*/
-}
-
-void writeCSV(){
-    FILE *file = fopen("record.csv", "w");
-    if (file == NULL) {
-    printf("Can not open file\n");
-    return;
-    }    
-    fclose(file);
-
 }
 
 //add
 void addRecord(){
-    int paymentID, payerName;
-    printf("Type the paymentID: \n");
-    scanf("%s", paymentID);
-    printf("Type the payer's name: \n");
-    scanf("%s", payerName);
+    if (countID > 100) {
+        printf("\nEnter name: ");
+        scanf(" %[^\n]s", I);
+
+
+    }
+    else {
+        printf("\nMaximum numbers of ID is reached\n");
+    }
 
 }
 
@@ -59,16 +60,23 @@ void search(){
 
 
 
+//Display Menu
+void displayMenu(){
+    printf("      Tax Management System\n");
+    printf("===============Menu===============\n");
+    printf("[1] Add the information\n");
+    printf("[2] Update the information\n");
+    printf("[3] Delete the data\n");  
+    printf("[4] Search(by paymentID)\n");
+    printf("[5] Unit test\n");
+    printf("[6] End to end test\n");
+    printf("[7] Exit\n");
+    printf("==================================\n");
+    printf("Enter your choice:\n");
+    scanf(%d, );
+}
+
 int main(){
-    readCSV();
-
-
-        displayMenu();
-        addRecord();
-
-
-
-
 
     return 0;
 }
